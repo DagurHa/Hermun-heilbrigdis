6a4a9322e1467a4d86941364bdb4bfec74f6d964
DagurHa <100678896+DagurHa@users.noreply.github.com>
2023-08-09 02:11:24 +0100

    hund

--- SimProj/Run.cs
+++ SimProj/Run.cs
@@ -8,14 +8,14 @@ public class Run
     public static bool upphitunFlag = false;
     public static void Main(string[] args)
     {
-        string pth = "./SimProj/bin/Release/net7.0/";
-        string simString_tup = File.ReadAllText(pth+"InputTuple.json");
-        string simString_nontup = File.ReadAllText(pth+"InputNonTuple.json");
+        //string pth = "../SimProj/bin/Release/net7.0/";
+        string simString_tup = File.ReadAllText("./InputTuple.json");
+        string simString_nontup = File.ReadAllText("./InputNonTuple.json");
         SimAttribs simAttr = Helpers.InitSimAttr(simString_tup, simString_nontup);
         simAttr.Log();
         TotalData retData = hermHundur(simAttr);
         var jsonRetString = JsonConvert.SerializeObject(retData,Formatting.Indented);
-        string jsonPth = pth + "JSONOUTPUT.json";
+        string jsonPth = "./JSONOUTPUT.json";
         File.WriteAllText(jsonPth, jsonRetString);
     }
     //Fall sem hermir kerfið einu sinni með völdum stillingum.
